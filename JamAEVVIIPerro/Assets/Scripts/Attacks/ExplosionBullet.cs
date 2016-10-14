f6227739a8338f79f96808c7f4f5810a129549f5
Ricardo de la rosa vivas <ricardodelarosavivas@gmail.com>
2016-10-15 01:50:55 +0200

    Gui manager activado

--- JamAEVVIIPerro/Assets/Scripts/Attacks/ExplosionBullet.cs
+++ JamAEVVIIPerro/Assets/Scripts/Attacks/ExplosionBullet.cs
@@ -1,17 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ForwardBullet : MonoBehaviour {
+public class ExplosionBullet : MonoBehaviour {
 
 
     public float bulletSpeed = 6f;
-    public float damage = 10f;
+    public float damage = 50f;
 
-
+    
+	
 	// Update is called once per frame
 	void Update () {
-
         transform.Translate(0, Time.deltaTime * bulletSpeed, 0);
+	
 
 	}
 }
