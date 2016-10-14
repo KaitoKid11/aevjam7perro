aad21f3ad50804082f0b8d59acbce600a2d48084
Ricardo de la rosa vivas <ricardodelarosavivas@gmail.com>
2016-10-15 00:17:54 +0200

    moviemiento de player bloqueado por los bordes

--- JamAEVVIIPerro/Assets/Scripts/PlayerMovement.cs
+++ JamAEVVIIPerro/Assets/Scripts/PlayerMovement.cs
@@ -6,32 +6,34 @@ public class PlayerMovement : MonoBehaviour {
 
     public float m_playerSpeed = 5;
 
-	void Start () {
-	
-	}
-	
+    [Header("Borders")]
+    public float minX = -7f;
+    public float maxX = 6.56f;
+
+    public float minY = -4.64f;
+    public float maxY = 4.42f;
 	void Update () {
 
 
-        if (Input.GetButton(KeyCodes.Up))
+        if (Input.GetButton(KeyCodes.Up) && transform.position.y < maxY)
         {
             transform.Translate(0, Time.deltaTime * m_playerSpeed, 0);
 
         }
 
-        if (Input.GetButton(KeyCodes.Down))
+        if (Input.GetButton(KeyCodes.Down) && transform.position.y > minY)
         {
             transform.Translate(0, Time.deltaTime * -m_playerSpeed, 0);
 
         }
 
-        if (Input.GetButton(KeyCodes.Left))
+        if (Input.GetButton(KeyCodes.Left) && transform.position.x > minX)
         {
             transform.Translate(Time.deltaTime * -m_playerSpeed, 0, 0);
 
         }
 
-        if (Input.GetButton(KeyCodes.Right))
+        if (Input.GetButton(KeyCodes.Right) && transform.position.x < maxX)
         {
             transform.Translate(Time.deltaTime * m_playerSpeed, 0, 0);
 
