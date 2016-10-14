508f49608e68fe9230717000f94ad32abfb55385
KaitoKid1111 <daleorei@gmail.com>
2016-10-15 01:52:35 +0200

    YO SOY UN PUTO MACARRA!

--- JamAEVVIIPerro/Assets/Scripts/Detection.cs
+++ JamAEVVIIPerro/Assets/Scripts/Detection.cs
@@ -3,10 +3,19 @@ using System.Collections;
 
 public class Detection : MonoBehaviour {
 
-    public Collider2D bloque;
-
     void OnTriggerExit2D(Collider2D other)
     {
-        
+        if (other.tag == Tags.Bloque && this.name == Tags.NameSpawn)
+        {
+            transform.parent.transform.parent.GetComponent<SpawnBlock>().SpawnBlockUnit(this.transform);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == Tags.Bloque && this.name == Tags.NameDelete)
+        {
+            transform.parent.transform.parent.GetComponent<SpawnBlock>().DeleteBlockUnit(other);
+        }
     }
 }
