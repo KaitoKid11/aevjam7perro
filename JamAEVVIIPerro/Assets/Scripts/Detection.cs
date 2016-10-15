a81e0d6612fd8733a54d7ce8a90fbb3d0c1f0217
KaitoKid1111 <daleorei@gmail.com>
2016-10-15 16:28:05 +0200

    Carga bloques largos

--- JamAEVVIIPerro/Assets/Scripts/Detection.cs
+++ JamAEVVIIPerro/Assets/Scripts/Detection.cs
@@ -2,12 +2,14 @@
 using System.Collections;
 
 public class Detection : MonoBehaviour {
+    
+    public int index;
 
     void OnTriggerExit2D(Collider2D other)
     {
         if (other.tag == Tags.Bloque && this.name == Tags.NameSpawn)
         {
-            transform.parent.transform.parent.GetComponent<SpawnBlock>().SpawnBlockUnit(this.transform);
+            transform.parent.transform.parent.GetComponent<SpawnBlock>().SpawnBlockUnit(this.transform, index);
         }
 
         if (other.tag == Tags.Intro && this.name == Tags.NameDelete)
--- JamAEVVIIPerro/Assets/Scripts/SpawnBlock.cs
+++ JamAEVVIIPerro/Assets/Scripts/SpawnBlock.cs
@@ -7,7 +7,7 @@ public class SpawnBlock : MonoBehaviour {
     public Sprite[] spritesBasicos = new Sprite[7];
     public Sprite[] spritesLargos = new Sprite[1];
 
-    private int lastUsed;
+    private int[] lastUsed = new int[7];
 	// Use this for initialization
 	void Start () {
         for (int i = 1; i < 8; ++i)
@@ -15,7 +15,7 @@ public class SpawnBlock : MonoBehaviour {
             int sprite = Random.Range(0, spritesBasicos.Length);
             bloque.GetComponent<SpriteRenderer>().sprite = spritesBasicos[sprite];
             Instantiate(bloque, transform.FindChild("Building" + i + "/Spawn").transform.position, Quaternion.identity);
-            lastUsed = sprite; 
+            lastUsed[i-1] = sprite; 
         }
 	}
 	
@@ -23,20 +23,20 @@ public class SpawnBlock : MonoBehaviour {
 	void Update () {
 	}
 
-    public void SpawnBlockUnit(Transform spawn)
+    public void SpawnBlockUnit(Transform spawn, int index)
     {
-        if (lastUsed == 6)
+        if (lastUsed[index] == 6)
         {
             bloque.GetComponent<SpriteRenderer>().sprite = spritesLargos[0];
             Instantiate(bloque, spawn.position, Quaternion.identity);
-            lastUsed = 0;
+            lastUsed[index] = 0;
         }
         else
         {
             int sprite = Random.Range(0, spritesBasicos.Length);
             bloque.GetComponent<SpriteRenderer>().sprite = spritesBasicos[sprite];
             Instantiate(bloque, spawn.position, Quaternion.identity);
-            lastUsed = sprite;
+            lastUsed[index] = sprite;
         }
     }
 
